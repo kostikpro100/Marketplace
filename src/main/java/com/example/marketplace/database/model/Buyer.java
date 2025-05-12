ead6572a29f92f1392826e436f0b8fce64d9bbac
kostik <kostyamigali2007@gmail.com>
2025-05-13 01:48:55 +0300

    buyer entity

--- src/main/java/com/example/marketplace/database/model/Buyer.java
+++ src/main/java/com/example/marketplace/database/model/Buyer.java
@@ -1,10 +1,52 @@
 package com.example.marketplace.database.model;
 
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+/*
+create table buyer (
+buyer_id bigserial primary key,
+buyer_login varchar(256),
+buyer_pwd varchar(256),
+buyer_nick varchar(256)
+)
+
+create table buyer_preferences (
+buyer_id bigint references buyer(buyer_id),
+category_id bigint references category(category_id),
+primary key(buyer_id, category_id)
+)
+ */
+
+@Table(name = "buyer")
+@Entity
 public class Buyer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "buyer_id")
+    private Long id;
+    @Column(name = "buyer_login")
     private String login;
+    @Column(name = "buyer_pwd")
     private String password;
+    @Column(name = "buyer_nick")
     private String nickname;
-    private Category[] preferences;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "buyer_preferences",
+            joinColumns = @JoinColumn(name = "buyer_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> preferences;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getLogin() {
         return login;
@@ -30,11 +72,11 @@ public class Buyer {
         this.nickname = nickname;
     }
 
-    public Category[] getPreferences() {
+    public Set<Category> getPreferences() {
         return preferences;
     }
 
-    public void setPreferences(Category[] preferences) {
+    public void setPreferences(Set<Category> preferences) {
         this.preferences = preferences;
     }
 }
