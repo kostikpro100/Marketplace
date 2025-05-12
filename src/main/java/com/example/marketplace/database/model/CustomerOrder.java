7fc3f189a418cc6fde57ceb66cb3c46125cc890c
kostik <kostyamigali2007@gmail.com>
2025-05-13 02:39:51 +0300

    orders entity

---
+++ src/main/java/com/example/marketplace/database/model/CustomerOrder.java
@@ -0,0 +1,42 @@
+package com.example.marketplace.database.model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+/*
+create table customer_order (
+order_id bigserial primary key,
+buyer_id bigint references buyer(buyer_id)
+)
+ */
+
+@Table(name = "customer_order")
+@Entity
+public class CustomerOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id")
+    private Buyer buyer;
+    @OneToMany(mappedBy = "order")
+    private Set<ProductOrder> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Buyer getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(Buyer buyer) {
+        this.buyer = buyer;
+    }
+}
--- src/main/java/com/example/marketplace/database/model/ProductOrder.java
+++ src/main/java/com/example/marketplace/database/model/ProductOrder.java
@@ -1,15 +1,46 @@
 package com.example.marketplace.database.model;
 
-public class ProductOrder {
-    private OrderID orderID;
-    private Product product;
+import jakarta.persistence.*;
 
-    public OrderID getOrderID() {
-        return orderID;
+/*
+create table product_order (
+product_order_id bigserial primary key,
+customer_order_id bigint references customer_order(order_id),
+product_id bigint references product(product_id),
+product_price bigint
+)
+ */
+
+@Table(name = "product_order")
+@Entity
+public class ProductOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_order_id")
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_order_id")
+    private CustomerOrder order;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @Column(name = "product_price")
+    private Long price;
+
+    public Long getId() {
+        return id;
     }
 
-    public void setOrderID(OrderID orderID) {
-        this.orderID = orderID;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CustomerOrder getOrder() {
+        return order;
+    }
+
+    public void setOrder(CustomerOrder order) {
+        this.order = order;
     }
 
     public Product getProduct() {
@@ -18,5 +49,13 @@ public class ProductOrder {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
     }
 }
