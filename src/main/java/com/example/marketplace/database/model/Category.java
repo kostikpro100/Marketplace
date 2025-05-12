461bd176b50b01f60618418c5a5018c8a2450a1b
kostik <kostyamigali2007@gmail.com>
2025-05-12 22:49:24 +0300

    category entity

--- src/main/java/com/example/marketplace/database/model/Category.java
+++ src/main/java/com/example/marketplace/database/model/Category.java
@@ -1,13 +1,37 @@
 package com.example.marketplace.database.model;
 
-public class Category {
-    private String category;
+import jakarta.persistence.*;
 
-    public String getCategory() {
-        return category;
+/*
+create table category (
+category_id bigserial primary key,
+category_name varchar(256)
+)
+ */
+
+@Table(name = "category")
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+    @Column(name = "category_name")
+    private String name;
+
+    public Long getId() {
+        return id;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
